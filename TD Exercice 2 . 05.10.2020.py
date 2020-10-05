b50f07e61d75cafb2261f6ceaaf35a7feef015ea
lilyhuong <44270876+lilyhuong@users.noreply.github.com>
2020-10-05 14:00:05 +0200

    Add files via upload

---
+++ TD Exercice 2 . 05.10.2020.py
@@ -0,0 +1,24 @@
+#Exercice1
+list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+print (list[:4])
+print (list[5:])
+#Exercice2
+print (list[6])
+print (list[-1])
+#Exercice3
+list.reverse()
+print(list)
+#Exercice4[]
+Spring = ["January", "February", "March" ]
+Summer = [ "April", "May", "June"]
+Autumn = ["July", "August", "September"]
+Winter = [ "October", "November", "December"]
+Season = [Spring, Summer, Autumn, Winter]
+print (Season[2])
+print (Season[1][0])
+print (Season[1:2])
+print (Season[:][1])
+#Exercice5
+print(len(range(2,10002,2)))
+
+
